b14d4aae4c51ab9481bbccb041cad05abbee2d40
shayandrg <shayan.derogarian@gmail.com>
2022-06-18 23:52:30 +0430

    some vuetify and tailwind and font and css stuff and configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,86 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s - cryptomarket',
-    title: 'cryptomarket',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		titleTemplate: '%s - CryptoMarket',
+		title: 'CryptoMarket',
+		htmlAttrs: {
+			lang: 'en'
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' }
+		],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [
+		'~/assets/css/main.scss'
+	],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [
+	],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		'@nuxtjs/vuetify',
+		'@nuxtjs/tailwindcss',
+	],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+	],
 
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
+	// Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+	vuetify: {
+		treeShake: true,
+		rtl: true,
+		customVariables: ['~/assets/variables.scss'],
+		theme: {
+			dark: true,
+			themes: {
+				dark: {
+					primary: colors.red.accent3,
+					secondary: colors.blueGrey.darken2,
+				},
+				light: {
+					primary: '#ad9742',
+					secondary: colors.blueGrey.darken1,
+				}
+			}
+		},
+		// defaultAssets: {
+		//     font: false,
+		//     icons: false
+		// }
+	},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+	tailwindcss: {
+		cssPath: '~/assets/css/tailwind.css',
+		configPath: 'tailwind.config.js',
+		exposeConfig: false,
+		injectPosition: 0,
+		viewer: false,
+		config: {
+			mode: 'jit',
+			theme: {
+				screens: {
+					sm: '600px',
+					md: '960px',
+					lg: '1264px',
+					xl: '1904px'
+				},
+			},
+		}
+	},
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {
+	}
 }
